88f1f57f7d882ee6bed908a620815589f5e356bd
Lázaro Eduardo da Silva <lazarodu@gmail.com>
2020-05-07 14:47:49 -0300

    Teste de Commit

--- database/migrations/1503250034280_token.js
+++ database/migrations/1503250034280_token.js
@@ -1,23 +1,26 @@
-'use strict'
-
+"use strict";
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class TokensSchema extends Schema {
-  up () {
-    this.create('tokens', (table) => {
-      table.increments()
-      table.integer('id_usuario').unsigned().references('id_usuario').inTable('usuarios')
-      table.string('token', 255).notNullable().unique().index()
-      table.string('type', 80).notNullable()
-      table.boolean('is_revoked').defaultTo(false)
-      table.timestamps()
-    })
+  up() {
+    this.create("tokens", (table) => {
+      table.increments();
+      table
+        .integer("id_usuario")
+        .unsigned()
+        .references("id_usuario")
+        .inTable("usuarios");
+      table.string("token", 255).notNullable().unique().index();
+      table.string("type", 80).notNullable();
+      table.boolean("is_revoked").defaultTo(false);
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('tokens')
+  down() {
+    this.drop("tokens");
   }
 }
 
-module.exports = TokensSchema
+module.exports = TokensSchema;
