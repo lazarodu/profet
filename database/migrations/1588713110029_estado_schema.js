1ae315616a80d47346209c3096b6f1b37d4c05fe
Lázaro Eduardo da Silva <lazarodu@gmail.com>
2020-05-07 14:44:45 -0300

    Teste de Commit

--- database/migrations/1588713110029_estado_schema.js
+++ database/migrations/1588713110029_estado_schema.js
@@ -1,21 +1,20 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class EstadoSchema extends Schema {
-  up () {
-    this.create('estados', (table) => {
-      table.increments('id_estado').primary()
-      table.string('estado', 45).notNullable()
-
-      table.timestamps()
-    })
+  up() {
+    this.create("estados", (table) => {
+      table.increments("id_estado").primary();
+      table.string("estado", 45).notNullable();
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('estados')
+  down() {
+    this.drop("estados");
   }
 }
 
-module.exports = EstadoSchema
+module.exports = EstadoSchema;
