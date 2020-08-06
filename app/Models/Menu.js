799baf21d93e80d4ff97f4c6dd079930dc3d89f6
Lázaro Eduardo da Silva <lazarodu@gmail.com>
2020-08-06 15:15:58 -0300

    primary key

--- app/Models/Menu.js
+++ app/Models/Menu.js
@@ -1,16 +1,19 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class Menu extends Model {
-    categorias() {
-        return this.hasMany("App/Models/Categoria", "id_menu", "id_menu");
-    }
+  categorias() {
+    return this.hasMany("App/Models/Categoria", "id_menu", "id_menu");
+  }
 
-    static get hidden() {
-        return ["created_at", "updated_at"];
-    }
+  static get hidden() {
+    return ["created_at", "updated_at"];
+  }
+  static get primaryKey() {
+    return "id_menu";
+  }
 }
 
-module.exports = Menu
+module.exports = Menu;
